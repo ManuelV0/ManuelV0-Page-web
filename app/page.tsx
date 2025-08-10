ff6c0e67b68da98a168d67147338016ae3825435
ManuelV0 <manuel.voll69@icloud.com>
2025-08-10 23:44:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-export default function Home() {
-  return (
-    <section className="hero-section">
-      <div className="hero-content">
-        <h1>Le Voci Più Amate del Mese!</h1>
-        <p>Scopri i talenti emergenti della community.</p>
-      </div>
-    </section>
-  );
+import PoetryHome from './(site)/components/PoetryHome'
+
+export default function Page() {
+  return <PoetryHome />
 }
