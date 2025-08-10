d837cb757ea6db8186d2676e1704d6ae74052fb2
ManuelV0 <manuel.voll69@icloud.com>
2025-08-10 23:21:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
-export const metadata = { title: 'TheItalianPoetry' }
-
 import './globals.css'
-import '../styles/style.css'   // il tuo design system
-import '../styles/diario.css'  // css della sezione diario
+import './diario.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
-      <body>
-        <header className="main-header">
-          <div className="logo">
-            <i className="fa-solid fa-feather-pointed" />
-            <span>TheItalianPoetry</span>
-          </div>
-        </header>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
