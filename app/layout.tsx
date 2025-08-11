88a51c093cd24d365d931212d5229057e220bcef
ManuelV0 <manuel.voll69@icloud.com>
2025-08-11 03:01:17 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import SiteHeader from '@/components/SiteHeader'
-import SiteFooter from '@/components/SiteFooter'
 
-export const metadata: Metadata = {
-  title: 'TheItalianPoetry',
-  description: 'Community e diario autori'
-}
+// app/layout.tsx
+
+import '../styles/style.css'
+import '../styles/diario.css'
+import '../styles/poetry-widget.css'
+
+
+export const metadata = { title: 'TheItalianPoetry' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
-      <body>
-        <SiteHeader />
-        {children}
-        <SiteFooter />
-      </body>
+      <head>
+        {/* roba esterna ok via <link> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&family=Open+Sans:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
