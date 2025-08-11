556e7218e81592374535afe1ca59c8eb4b09bff2
ManuelV0 <manuel.voll69@icloud.com>
2025-08-11 02:43:45 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,12 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-if (!url || !anon) {
-  // utile in build logs Netlify
-  console.warn('Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY')
-}
-
-export const supabase = createClient(url, anon)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
