677bec5d224822dea18bad10d40cf2ee01816b60
ManuelV0 <manuel.voll69@icloud.com>
2025-08-11 02:35:40 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-};
-export default nextConfig;
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /@supabase\/realtime-js/, message: /Critical dependency:/ },
+    ];
+    return config;
+  },
+}
+
+export default nextConfig
