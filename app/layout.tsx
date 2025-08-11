bf34afcadb694ec8bdbfbe165ceaf98e9d7c039c
ManuelV0 <manuel.voll69@icloud.com>
2025-08-11 02:03:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import './globals.css'
-import './diario.css'
+// app/layout.tsx
+import '../css/style.css';
+import '../css/diario.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'TheItalianPoetry',
+  description: 'La community della poesia',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
       <body>{children}</body>
     </html>
-  )
+  );
 }
