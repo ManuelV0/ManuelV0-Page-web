bc68c8cfd13e3c5cae998cc2af7ce30079a5b9f4
ManuelV0 <manuel.voll69@icloud.com>
2025-08-11 03:49:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 
-import './app/globals.css'
+import '../app/globals.css'
 import Link from 'next/link'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
