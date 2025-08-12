3f649de18070e67103c192b4f4006988f9910d57
ManuelV0 <manuel.voll69@icloud.com>
2025-08-12 16:51:16 +0200

    Delete app directory

--- app/diario/metadata.ts
+++
@@ -1,6 +0,0 @@
-
-export const metadata = {
-  title: 'Diario Autore – TheItalianPoetry',
-  description: 'Profili poetici aggiornati, temi e opere recenti.',
-  openGraph: { title: 'Diario Autore', type: 'website' }
-}
